1a0a547a570655581a8cf82a896b1e088b574f91
Ludovic BOUÉ <lboue@users.noreply.github.com>
2025-11-06 23:17:01 +0100

    Implement nRF52 support in mDNS component
    Add support for nRF52 platform in mDNS component

--- esphome/components/mdns/__init__.py
+++ esphome/components/mdns/__init__.py
@@ -172,6 +172,8 @@ async def to_code(config):
             cg.add_library("ESP8266mDNS", None)
         elif CORE.is_rp2040:
             cg.add_library("LEAmDNS", None)
+        elif CORE.is_nrf52:
+            # TODO
 
     if CORE.is_esp32:
         add_idf_component(name="espressif/mdns", ref="1.9.1")
@@ -245,5 +247,6 @@ FILTER_SOURCE_FILES = filter_source_files_from_platform(
             PlatformFramework.RTL87XX_ARDUINO,
             PlatformFramework.LN882X_ARDUINO,
         },
+        "mdns_zephyr.cpp":: {PlatformFramework.ZEPHYR},
     }
 )
